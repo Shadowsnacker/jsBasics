7e6a67d733fb09ef9abb1c0482e402c6253fafe6
Jeremy Boyd <156616188+Shadowsnacker@users.noreply.github.com>
2026-01-15 20:30:29 -0600

    Continuing Lessons

--- index.js
+++ index.js
@@ -215,6 +215,19 @@ function Address(street, city, zipcode) {
     this.city = city;
     this.zipcode = zipcode;
 }
-
 // Exercise #14
+let address2 = new Address('a', 'b', 'c');
+let address3 = address1;
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+console.log(areSame(address1, address3));
+function areEqual(address1, address2) { // Are properties equal
+    return address1.street === address2.street &&
+        address1.city === address2.city &&
+        address1.zipcode === address2.zipcode
+}
+function areSame(address1, address2) { // Are the object locations equal
+    return address1 === address2;
+}
+
 // Exercise #15
