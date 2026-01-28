829ad26d8da5ca0b8178d92c88e8a865701a7489
Jeremy Boyd <156616188+Shadowsnacker@users.noreply.github.com>
2026-01-27 18:29:57 -0600

    Setters

--- index.js
+++ index.js
@@ -576,6 +576,12 @@ const person = {
     // fullName: 'Jeremy Boyd'
     get fullName() {
         return `${person.firstName} ${person.lastName}`;
+    },
+    set fullName(value){
+        const parts = value.split(' ');
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 };
-console.log(person.fullName);
+person.fullName = 'John Smith';
+console.log(person);
