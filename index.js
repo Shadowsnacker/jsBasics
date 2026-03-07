145e365ecb792d150a626112a4ff422238c17b28
Jeremy Boyd <156616188+Shadowsnacker@users.noreply.github.com>
2026-03-07 15:35:35 -0600

    intermediate function inheritance

--- index.js
+++ index.js
@@ -1031,14 +1031,16 @@ circle.draw(); */
 // const s = new Shape();
 // const c = new Circle(1);
 
-// // Calling the Super Constructor
+// // Calling the Super Constructor: 
+// // OUTPUT in dev tools in live server: type 'c', and the information inside the function will show the called placements
 // function Shape(color){
 //     this.color = color;
 // }
 // Shape.prototype.duplicate = function() {
 //     console.log('duplicate');
 // }
-// function Circle(radius) {
+// function Circle(radius, color) {
+//     Shape.call(this, color);
 //     this.radius = radius;
 // }
 // Circle.prototype = Object.create(Shape.prototype);
@@ -1047,5 +1049,32 @@ circle.draw(); */
 //     console.log('draw');
 // }
 // const s = new Shape();
-// const c = new Circle(1);
+// const c = new Circle(1, 'red');
+
+// Intermediate function inheritance(the extend function below)
+function Shape(color){
+    this.color = color;
+}
+Shape.prototype.duplicate = function() {
+    console.log('duplicate');
+}
+function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;  
+}
+function Circle(radius, color) {
+    Shape.call(this, color);
+    this.radius = radius;
+}
+extend(Circle, Shape);
+Circle.prototype.draw = function(){
+    console.log('draw');
+}
+function Square(size){
+    this.size = size;
+}
+extend(Square, Shape);
+
+const s = new Shape();
+const c = new Circle(1, 'red');
 
